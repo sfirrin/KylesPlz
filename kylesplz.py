84d32ad7fe7bcda3c007199b7597cbed11ca8cd6
Stephen Firrincieli <sfirrincieli@gmail.com>
2017-11-06 21:25:22 -0500

    Refactor for use in a lambda function

--- kylesplz.py
+++ kylesplz.py
@@ -1,66 +1,63 @@
-#!/usr/bin/env python
 import sys
-import random, os
+import random
+import os
 import time
 from twython import Twython, TwythonError
+from logzero import logger as log
 import string
 import random
 
 
-def main():
-    # your twitter consumer and access information goes here
+def tweet():
+    # Reading secret info from environment variables
+    apiKey = os.environ.get('API_KEY')
+    apiSecret = os.environ.get('API_SECRET')
+    accessToken = os.environ.get('ACCESS_TOKEN')
+    accessTokenSecret = os.environ.get('ACCESS_TOKEN_SECRET')
 
-    credentials = open('credentials.txt', 'rb')
-    apiKey = credentials.next().strip()
-    apiSecret = credentials.next().strip()
-    accessToken = credentials.next().strip()
-    accessTokenSecret = credentials.next().strip()
+    if not all([apiKey, apiSecret, accessToken, accessTokenSecret]):
+        print('You need to set up your environment variables before you can run this')
+        return
 
     twitter = Twython(apiKey, apiSecret, accessToken, accessTokenSecret)
 
     # GET FOLLOWERS
     followers = (twitter.get_followers_ids(screen_name="@kylesplz")['ids'])
 
-    print followers
+    log.info('Found these followers, {}'.format(followers))
 
     # pick a random ID from all followers
-    luckyid = random.choice(followers)
-
+    winner_id = random.choice(followers)
     # lookup the handle of the lucky ID
-    output = twitter.lookup_user(user_id=luckyid)
+    winner_user = twitter.lookup_user(user_id=winner_id)[0]
 
-    # Loop through the results (Twitter screen names)
-    for user in output:
-        print user['screen_name']
-        username_list = (user['screen_name'])
+    log.info('Lucky follower was {}'.format(winner_user))
 
-    print username_list
-
-    luckyfollower = username_list
-
-    # luckyfollower = (twitter.lookup_user(user_id=luckyid))
-    # print luckyfollower['screen_name']
-
-    # GET RANDOM COMPLIMENT AND TWEET AT PERSON
+    winner_handle = winner_user['screen_name']
 
     lines = open('tweet_options.txt').read().splitlines()
     random_line = random.choice(lines)
-    # print(myline)
 
-    path = r'kyles/'
+    images_path = r'kyles/'
 
-    random_pic = random.choice([x for x in os.listdir(path)
-                                if os.path.isfile(os.path.join(path, x))])
+    random_pic = random.choice([x for x in os.listdir(images_path)
+                                if os.path.isfile(os.path.join(images_path, x))])
 
-    picfind = path + random_pic
+    picfind = images_path + random_pic
 
-    message = 'Hey @' + luckyfollower + ', ' + random_line
+    message = 'Hey @{}, {}'.format(winner_handle, random_line)
 
+    log.info('Attempting tweet: {}'.format(message))
     with open(picfind, 'rb') as photo:
         twitter.update_status_with_media(status=message, media=photo)
 
-    print 'tweet complete'
+    log.info('Tweet successfully made')
+
+
+def lambda_handler(event, context):
+    print event, context
+    tweet()
 
 
 if __name__ == '__main__':
-    main()
+    tweet()
